8b2b03f7c0deb206975ade7b7afabb4a4af9a1f0
Yash Vardhan Shukla <lakkyshukla74339@gmail.com>
2025-10-11 23:34:25 +0530

    added liked and comments

---
+++ Backend/src/main/java/com/Flow/Backend/model/Comment.java
@@ -0,0 +1,51 @@
+package com.Flow.Backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Table(name = "comments")
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reply;
+    private String username;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    @JsonIgnore
+    private PostModel post;
+
+    public PostModel getPost() {
+        return post;
+    }
+
+    public void setPost(PostModel post) {
+        this.post = post;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- Backend/src/main/java/com/Flow/Backend/model/PostModel.java
+++ Backend/src/main/java/com/Flow/Backend/model/PostModel.java
@@ -4,6 +4,10 @@ package com.Flow.Backend.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "posts")
@@ -17,6 +21,31 @@ public class PostModel {
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @ManyToMany(mappedBy = "likedPosts")
+    private Set<UserModel> likedByUsers=new HashSet<>();
+
+
+    private int likes;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 
     private String imageUrl;
 
