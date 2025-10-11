84b33b39dade93c16a550866df87a830ca5f6b4d
Lakshya Varshney <gq0396@myamu.ac.in>
2025-10-11 13:17:58 +0530

    User Service working

--- Backend/src/main/java/com/Flow/Backend/service/UserService.java
+++ Backend/src/main/java/com/Flow/Backend/service/UserService.java
@@ -1,8 +1,36 @@
 package com.Flow.Backend.service;
 
+import com.Flow.Backend.DTO.RegisterBody;
+import com.Flow.Backend.model.UserModel;
+import com.Flow.Backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 
+    public String register(RegisterBody registerBody){
+        if (userRepository.findByEmail(registerBody.getEmail()).isPresent()) {
+            throw new UserAlreadyExistException("Email already exists");
+        }
+
+        if (userRepository.findByUsername(registerBody.getUsername()).isPresent()) {
+            throw new UserAlreadyExistException("Username already exists");
+        }
+        UserModel user=new UserModel();
+        user.setEmail(registerBody.getEmail());
+        user.setFirstName(registerBody.getFirstName());
+        user.setLastName(registerBody.getLastName());
+        user.setUsername(registerBody.getUsername());
+        user.setPassword(passwordEncoder.encode(registerBody.getPassword()));
+
+        UserModel savedUser = userRepository.save(user);
+        UserDetails userDetails = userDetailService.loadUserByUsername(savedUser.getUsername());
+        String jwt = jwtUtil.generateToken(savedUser.getUsername());
+
+        return jwt;
+    }
 }
