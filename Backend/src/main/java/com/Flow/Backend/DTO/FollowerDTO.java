00f48450b3b0f32a55c5db84c13aba53546443e0
Lakshya Varshney <gq0396@myamu.ac.in>
2025-10-11 18:57:09 +0530

    Get followers and following added

---
+++ Backend/src/main/java/com/Flow/Backend/DTO/FollowerDTO.java
@@ -0,0 +1,31 @@
+package com.Flow.Backend.DTO;
+
+public class FollowerDTO {
+    private Long id;
+    private String username;
+    private String profilePic;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+}
