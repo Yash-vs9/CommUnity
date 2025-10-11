6a66b937be75f4006d5b59f09fd978f389465ffd
Lakshya Varshney <gq0396@myamu.ac.in>
2025-10-11 13:36:38 +0530

    UserService login and Register completed

--- Backend/src/main/java/com/Flow/Backend/service/UserService.java
+++ Backend/src/main/java/com/Flow/Backend/service/UserService.java
@@ -1,13 +1,19 @@
 package com.Flow.Backend.service;
 
+import com.Flow.Backend.DTO.LoginBody;
 import com.Flow.Backend.DTO.RegisterBody;
+import com.Flow.Backend.exceptions.UserAlreadyExistException;
 import com.Flow.Backend.filter.JwtFilter;
 import com.Flow.Backend.model.MyUserDetailService;
 import com.Flow.Backend.model.UserModel;
 import com.Flow.Backend.repository.UserRepository;
 import com.Flow.Backend.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +27,8 @@ public class UserService {
     private JwtUtils jwtUtils;
     @Autowired
     private MyUserDetailService myUserDetailService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
     public String register(RegisterBody registerBody){
         if (userRepository.findByEmail(registerBody.getEmail()).isPresent()) {
             throw new UserAlreadyExistException("Email already exists");
@@ -42,5 +50,20 @@ public class UserService {
 
         return jwt;
     }
-    public String login
+    public String login(LoginBody loginBody){
+        UserModel user=userRepository.findByEmail(loginBody.getEmail())
+                .orElseThrow(()-> new UsernameNotFoundException("This email is not registered"));
+        String username=user.getUsername();
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username,loginBody.getPassword()));
+        } catch (Exception e){
+            throw new BadCredentialsException("Password Incorrect");
+        }
+        UserDetails userDetails= myUserDetailService.loadUserByUsername(username);
+        String jwt=jwtUtils.generateToken(userDetails.getUsername());
+        return jwt;
+
+    }
+
 }
