72e6e7e83e30119cc04023e493852101bfaf2b93
Lakshya Varshney <gq0396@myamu.ac.in>
2025-10-12 13:57:23 +0530

    All community added

---
+++ Backend/src/main/java/com/Flow/Backend/DTO/AllCommunityDTO.java
@@ -0,0 +1,40 @@
+package com.Flow.Backend.DTO;
+
+public class AllCommunityDTO {
+    private Long id;
+    private String name;
+    private String logoUrl;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
