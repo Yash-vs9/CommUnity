1e2b10cdc7abdbda6dfa01d0246f99aaea636930
Lakshya Varshney <gq0396@myamu.ac.in>
2025-10-13 02:07:21 +0530

    Events completed

--- Backend/src/main/java/com/Flow/Backend/repository/ChatRepository.java
+++ Backend/src/main/java/com/Flow/Backend/repository/ChatRepository.java
@@ -2,9 +2,10 @@ package com.Flow.Backend.repository;
 
 import com.Flow.Backend.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     Optional<Chat> findByUsername(String username);
 }
--- Backend/src/main/java/com/Flow/Backend/repository/PostRepository.java
+++ Backend/src/main/java/com/Flow/Backend/repository/PostRepository.java
@@ -2,9 +2,10 @@ package com.Flow.Backend.repository;
 
 import com.Flow.Backend.model.PostModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PostRepository extends JpaRepository<PostModel,Long> {
     List<PostModel> findByCreatedByUser(String createdByUser);
     // Fetch the latest 10 posts based on creation date
